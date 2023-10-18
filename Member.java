9e35974fb4e7537e08217e12da47f0f5b579a608
yhk <144309348+aaa0034213@users.noreply.github.com>
2023-10-18 16:43:48 +0900

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,17 @@
+package ex0003;
+
+public class Member {
+
+		String userId;
+		String password;
+		String username;
+		int age;
+		
+		Member(String userdId, String password,String username, int age){
+			this.userId=userId;
+			this.password=password;
+			this.username=username;
+			this.age=age;
+			
+		}
+}
